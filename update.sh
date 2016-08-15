dc13331829eb5b5162d8b4c4a2c89c1c07efe73f
Robert Gornall <robertgo@beam.tv>
2016-08-15 18:44:14 +0100

    Fix exec path for cli53

--- update.sh
+++ update.sh
@@ -6,7 +6,7 @@ URL='http://169.254.169.254/latest/meta-data/local-ipv4'
 MYIP=`curl -s $URL`
 
 if [[ $MYIP =~ ^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+$ ]]; then  
-  cli53 rrcreate --replace $ZONE "$RECORD A $MYIP"
+  ./cli53 rrcreate --replace $ZONE "$RECORD A $MYIP"
 else
   echo "Invalid IP returned from: $URL"
 fi
